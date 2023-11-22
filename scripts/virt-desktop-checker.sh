dc925bdce6875d82315cbdf88fdc32cc7775501d
Tycho Brouwer <tychobrouwer33@gmail.com>
2023-11-23 00:48:04 +0100

    fix startup

--- scripts/virt-desktop-checker.sh
+++ scripts/virt-desktop-checker.sh
@@ -5,6 +5,8 @@ interface=org.kde.KWin.VirtualDesktopManager
 object_path=/VirtualDesktopManager
 member=currentChanged
 
+printf '{"text": "        ", "class": "%s"}\n' "$(if [[ $desktop_nr == 1 ]]; then echo 'active'; else echo 'inactive'; fi)"
+
 dbus-monitor --session "interface='$interface',member='$member'" |
 while read -r line; do
   current_id=$(echo "$line" | grep "string" | cut -d '"' -f 2)
